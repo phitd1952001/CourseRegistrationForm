517e2d6142906ff3625ae4ee4db357634657386a
Đường Phi <phi1952001@gmail.com>
2025-09-01 11:00:14 +0700

    done

---
+++ Data/UserRegistrationVM.cs
@@ -0,0 +1,35 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+public class UserRegistrationVM
+{
+    [Required(ErrorMessage = "Full Name is required")]
+    [StringLength(50, MinimumLength = 3, ErrorMessage = "Full Name must be at least characters long.")]
+    public string FullName { get; set; }
+
+
+    [Required(ErrorMessage = "Email is required")]
+    [EmailAddress(ErrorMessage = "Please enter a valid email address")]
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "Phone number is required")]
+    [RegularExpression(@"^\d{10,12}$", ErrorMessage = "Phone number must be 10-12 digits")]
+    public string PhoneNumber { get; set; }
+
+    [Required(ErrorMessage = "Please select a Course.")]
+    public string Course { get; set; }
+
+    [Required(ErrorMessage = "Study mode is required")]
+    public bool? Study { get; set; } = true; // Mặc định Online
+
+    [Required(ErrorMessage = "Please select a start date.")]
+    public DateTime DateTime { get; set; }
+
+    [Required(ErrorMessage = "Comment is required")]
+    [StringLength(250, MinimumLength = 10 , ErrorMessage = "Comment must be at least 10 characters long.")]
+    public string Comment { get; set; }
+
+    [Required(ErrorMessage = "You must accept the terms")]
+    [Range(typeof(bool), "true", "true", ErrorMessage = "You must agree to the terms")]
+    public bool AcceptTerms { get; set; }
+}
--- obj/Debug/net9.0/FormRegistration.AssemblyInfo.cs
+++ obj/Debug/net9.0/FormRegistration.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FormRegistration")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e04c0b3ba924aee7178036164b2533808d1e1bd5")]
 [assembly: System.Reflection.AssemblyProductAttribute("FormRegistration")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FormRegistration")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
